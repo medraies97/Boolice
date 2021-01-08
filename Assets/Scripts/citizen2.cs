49ab8fdfc2e389ccbea2bc98a53718bc6888737f
medraies97 <76446557+medraies97@users.noreply.github.com>
2021-01-08 03:08:24 +0100

    Boolice

---
+++ Assets/Scripts/citizen2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class citizen2 : MonoBehaviour
+{
+    public float speed;
+    private bool go = true;
+    public int Limit = 33;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= Limit && go)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            if (transform.position.x > Limit)
+            {
+                go = !go;
+            }
+        }
+        else if (transform.position.x >= -Limit && !go)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+            if (transform.position.x < -Limit)
+            {
+                go = !go;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 a;
+    private float speed = 20;
+    public float velocity=6f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            a = Vector2.right;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            a = Vector2.left;
+        }
+        transform.Translate(speed * a * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
